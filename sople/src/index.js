e8d8f183810aaaae368508fc93a1aad0e8b4eb25
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-03-27 22:55:39 +0900

    엘리먼트 랜더링

--- sople/src/index.js
+++ sople/src/index.js
@@ -5,14 +5,24 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
+import Clock from "./chaper_04/Clock";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Library />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <Library />
+//   </React.StrictMode>
+// );
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+      <Library />
+    </React.StrictMode>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
